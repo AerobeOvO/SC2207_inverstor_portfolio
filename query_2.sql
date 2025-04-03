422e89b08c6b944735e1240aef3a8639b0cb8e8b
Rais <162988475+IM4plebz@users.noreply.github.com>
2025-04-03 12:17:16 +0800

    Update query_2.sql

--- query_2.sql
+++ query_2.sql
@@ -1,15 +1,18 @@
 -- QUERY 2: Find investors who are seeing an annualized return of more than 10% from their portfolios in 2024
 SELECT 
-    i.ID,
+    i.Phone,
     i.Name,
     i.Risk_Level,
     p.PID,
-    p.Annualized_Return_2024
+    p.Annualized_Return
 FROM 
     INVESTOR i
 JOIN 
-    PORTFOLIO p ON i.ID = p.Investor_ID
+    PORTFOLIO p ON i.Phone = p.Investor_PhoneNumber
+JOIN 
+    PORTFOLIO p ON m.Portfolio_ID = p.PID
 WHERE 
-    p.Annualized_Return_2024 > 10
+    p.Annualized_Return > 10,
+	m.Year = 2024
 ORDER BY 
-    p.Annualized_Return_2024 DESC;
+    p.Annualized_Return DESC;
